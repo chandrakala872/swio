4d2e2650ff1cb3280a03647536b055f0287abfcc
chandu <chanduduganti@gmail.com>
2024-05-22 16:55:15 +0530

    add stylings

--- sleevewebsite/src/Components/Integrations.js
+++ sleevewebsite/src/Components/Integrations.js
@@ -2,14 +2,12 @@
 
 
 import React from 'react';
+import './Integrations.css'
 
 const Integrations = () => (
   <div className="integrations">
     <h2>Like, Scrobble</h2>
-    <p>Sleeve integrates with Last.fm so you never miss a track. Simply sign in and start scrobbling. A Last.fm account is required.</p>
-    <p>With Sleeve 2 you can now like tracks in Spotify, without ever having to switch away from Sleeve. A free Spotify Developer account is required.</p>
-    <p>Sleeve integrates directly with Apple Music, Spotify, and Doppler. Control playback, like tracks, and adjust volume seamlessly.</p>
-    <a href="https://replay.software/sleeve">View the quick guide to connecting Sleeve to your Spotify account.</a>
+    
   </div>
 );
 
